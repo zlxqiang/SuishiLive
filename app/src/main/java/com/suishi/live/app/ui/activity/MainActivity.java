fadb81014167d5030b32aa46961a2a6dac3c9726
xiaoqiangyun <635933104@qq.com>
2019-01-19 20:22:12 +0800

    android stuido 3.0

--- app/src/main/java/com/suishi/live/app/ui/activity/MainActivity.java
+++ app/src/main/java/com/suishi/live/app/ui/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.suishi.live.app.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
